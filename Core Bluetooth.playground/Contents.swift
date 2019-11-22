80d13f18136014db81d47dd5e52db91d1d8c9f8b
Kyle Brodie <kyle@pkcsecurity.com>
2019-11-22 12:41:49 -0800

    Connect to OBDBLE and print all services, characteristics, descriptors

--- Core Bluetooth.playground/Contents.swift
+++ Core Bluetooth.playground/Contents.swift
@@ -4,22 +4,88 @@ import PlaygroundSupport
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-class BLEDiscovery: NSObject, CBCentralManagerDelegate {
+class BLEDiscovery: NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    
+    var myManager: CBCentralManager? = nil
+    var myPeripheral: CBPeripheral? = nil
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let e = error {
+            print(e)
+        }
+        else if let v = characteristic.value {
+            print(v)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor descriptor: CBDescriptor, error: Error?) {
+        if let e = error {
+            print(e)
+        }
+        else if let v = descriptor.value {
+            print(v)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverDescriptorsFor characteristic: CBCharacteristic, error: Error?) {
+        print("didDiscoverDescriptorsFor:")
+        if let e = error {
+            print(e)
+        }
+        else if let descriptors = characteristic.descriptors {
+            for descriptor in descriptors {
+                print(descriptor)
+                peripheral.readValue(for: descriptor)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        print("didDiscoverCharacteristicsFor:")
+        if let e = error {
+            print(e)
+        }
+        else if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                print(characteristic)
+                peripheral.discoverDescriptors(for: characteristic)
+                peripheral.readValue(for: characteristic)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("didDiscoverServices:")
+        if let e = error {
+            print(e)
+        }
+        else if let services = peripheral.services {
+            for service in services {
+                print(service)
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("didConnect:", peripheral)
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
     }
+    
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         print("didUpdateState:")
         switch central.state {
             case .poweredOn:
                 print("on")
+                self.myManager = central
                 central.scanForPeripherals(withServices: nil, options: nil)
-                print(central.isScanning)
             default:
                 print(central.state.rawValue)
                 break
         }
     }
+    
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         if let e = error {
             print("didFailToConnect:", peripheral, e)
@@ -28,9 +94,16 @@ class BLEDiscovery: NSObject, CBCentralManagerDelegate {
             print("didFailToConnect:", peripheral)
         }
     }
+    
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("didDiscoveradvertisementDatarssi:", peripheral, advertisementData, RSSI)
+        if peripheral.identifier == UUID(uuidString:"722E5F19-61B4-4A74-9500-84A41F943DCB") {
+            print("advertisementData:", advertisementData)
+            self.myPeripheral = peripheral
+            central.connect(peripheral, options: nil)
+            central.stopScan()
+        }
     }
+    
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         if let e = error {
             print("didDisconnectPeripheral:", peripheral, e)
